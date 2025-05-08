ca46fe25f04d1ea3312c97285c341fe1140d6fbb
Alejandro Rueda <alejandro.rueda@nazaries.com>
2025-05-08 13:28:49 +0300

    Time zone test errors fixed

--- spec/system/campaign_in_progress_spec.rb
+++ spec/system/campaign_in_progress_spec.rb
@@ -61,13 +61,13 @@ describe "Show campaign" do
 
     context "when less than 1 day left" do
       before do
-        Timecop.freeze(Date.parse(data["calendar"]["optimum"]) - 12.hours)
+        Timecop.freeze(Time.zone.parse(data["calendar"]["optimum"]) - 12.hours)
         visit_component
       end
 
       it "displays the hours left" do
         within ".thermometer-info .date" do
-          expect(page).to have_content("Only 9 hours!")
+          expect(page).to have_content("Only 12 hours!")
         end
       end
     end
